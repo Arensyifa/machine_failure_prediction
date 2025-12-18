c56b925d01ab904d0ba32d90fe7f6acfc851a3c6
Arensyifa <arensyifa029@gmail.com>
2025-12-18 22:11:50 +0700

    Final submission folder added

---
+++ SMSML_ArenSyifa/Membangun_model/modelling_tuning.py
@@ -0,0 +1,60 @@
+import mlflow
+import pandas as pd
+import dagshub
+import matplotlib.pyplot as plt
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+import json
+import joblib
+
+# Init DagsHub
+dagshub.init(repo_owner="Arensyifa", repo_name="machine_failure_prediction", mlflow=True)
+
+X_train = pd.read_csv('maintenance_preprocessing/X_train.csv')
+y_train = pd.read_csv('maintenance_preprocessing/y_train.csv')
+X_test = pd.read_csv('maintenance_preprocessing/X_test.csv')
+y_test = pd.read_csv('maintenance_preprocessing/y_test.csv')
+
+mlflow.set_experiment("Predictive_Maintenance_Tuning")
+
+with mlflow.start_run():
+    params = {"n_estimators": 100, "max_depth": 6, "learning_rate": 0.1}
+    mlflow.log_params(params)
+    
+    model = XGBClassifier(**params)
+    model.fit(X_train, y_train)
+    
+    # Metrics
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("accuracy", acc)
+    print(f"Berhasil! Akurasi Model: {acc}")
+    
+    # Artifact: Confusion Matrix
+    cm = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.savefig("confusion_matrix.png")
+    mlflow.log_artifact("confusion_matrix.png")
+    
+    # Artifact: Feature Importance
+    plt.figure()
+    # Memberi label pada feature importance
+    feat_importances = pd.Series(model.feature_importances_, index=X_train.columns)
+    feat_importances.nlargest(10).plot(kind='barh')
+    plt.title("Feature Importance")
+    plt.savefig("feature_importance.png")
+    mlflow.log_artifact("feature_importance.png")
+    
+    # Extra Artifacts
+    with open("metric_info.json", "w") as f:
+        json.dump({"status": "completed", "threshold_met": True}, f)
+    mlflow.log_artifact("metric_info.json")
+    
+    mlflow.sklearn.log_model(model, "model")
+
+# Baris ini akan memaksa laptopmu membuat file model.pkl di folder utama
+joblib.dump(model, 'model.pkl') 
+print("File model.pkl berhasil dibuat di folder utama!")
+
+print("Eksperimen selesai. Silakan cek DagsHub!")
